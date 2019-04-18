b09bfd3847d2546ba5f14cca7a4b4483ecc05ae3
jamcry <jamcry@hotmail.com>
2019-04-18 11:49:02 +0300

    Add settings (show one or all tweets)

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 import tweepy
 import json
+import sys
+showType = "all"
+def getSysArg():
+    #returns the commandline argument, or pass if not given
+    try:
+        return sys.argv[1]
+    except IndexError:
+        pass
+
+option = getSysArg()
+if option == "--settings":
+    print("## TT SETTINGS")
+    print("# 1. Show all tweets")
+    print("# 2. Show tweets one by one")
+    while True:
+        choice = input(" Select one ('q' to exit): ")
+        if choice == "1":
+            showType = "all"
+            break
+        elif choice == "2":
+            showType = "one"
+            break
+        elif choice == "q":
+            print (" You didn't change the settings.")
+            break
+        else:
+            print(" Invalid choice. Enter 'q' to exit.")
 
 try:
-    # Load app keys from json file
     with open("keys.json", "r") as f:
+        # Load developer keys from json file
         keys = json.load(f)
 except FileNotFoundError:
     print("[!] THE json FILE NOT FOUND!")
@@ -20,8 +47,12 @@ except tweepy.TweepError as e:
     exit(0)
 
 print (f" API User Name: {user.name}")
+
 homeFeed = api.home_timeline()
 for tweet in homeFeed:
     print("-" * 50)
     print(f" @{tweet.user.name} :")
     print(f"  {tweet.text}")
+    if showType == "one":
+        choice = input(" Press any key for next tweet: ")
+        if choice == "q": exit(0)
